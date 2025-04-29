a10d904d96a865ab109a88eb0c9ec4ec646c64dc
Zimon23 <95996947+Zimon23@users.noreply.github.com>
2025-04-29 10:40:26 +0900

    123

--- JavaStack/src/com/JavaStack/UI/Menu.java
+++ JavaStack/src/com/JavaStack/UI/Menu.java
@@ -7,19 +7,26 @@ public class Menu {
 		System.out.println("=======================");
 		System.out.println("오라클 인터넷뱅킹 App v1.5");
 		System.out.println("=======================");
-		System.out.println("1. 테이블 생성");
-		System.out.println("2. 테이블 삭제");
-		System.out.println("3. 데이터 추가");
-		System.out.println("4. 데이터 보기");
-		System.out.println("5. 데이터 추가(랜덤)");
-		System.out.println("6. 데이터 수정");
-		System.out.println("7. 데이터 삭제");
-		System.out.println("8. 데이터 검색");
+		System.out.println("1. 지출/수입 내역 등록");
+		System.out.println("2. 지출/수입 내역 삭제");
+		System.out.println("3. 지출/수입 내역 수정");
+		System.out.println("4. 지출/수입 내역 보기");
+		System.out.println("5. 잔액 보기");
+		System.out.println("6. 카테고리 추가");
+		System.out.println("7. 카테고리 삭제");
+		System.out.println("8. 카테고리 수정");
 		System.out.println("9. 종료");
+		System.out.println("10. 카테고리 보기");
 		System.out.println("=======================");
 		System.out.print("메뉴 선택:");
 		int m = s.nextInt();
 		return m;
+				
+
+		
+		
+		
+		
 	}
 	
 	public int updateMenu(Scanner s) {
