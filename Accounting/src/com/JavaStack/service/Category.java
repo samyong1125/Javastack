484261e788f5c98f0ab0f5ec9bb6003fee2ddf86
deceit03 <wop359@gmail.com>
2025-04-30 13:54:34 +0900

    11s

---
+++ Accounting/src/com/JavaStack/service/Category.java
@@ -0,0 +1,15 @@
+package com.JavaStack.service;
+
+import com.JavaStack.model.Member;
+
+public class Category {
+
+	public Category(Member mb) {
+		super();
+		// TODO Auto-generated constructor stub
+		mb.getMemberId()
+		
+	}
+
+	
+}
